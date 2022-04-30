e82c8ec0b24fd7a1bf1913be04d44d9724ff1689
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-04-30 11:39:27 +0600

    create all the folder and file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
 
+import app from "./firebase.init";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth(app);
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleClick = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        // ...
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-     <h1>This is sakib</h1>
+      <button onClick={handleClick}>click</button>
     </div>
   );
 }
---
+++ src/pages/MyItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyItem = () => {
+  return (
+    <div>MyItem</div>
+  )
+}
+
+export default MyItem
